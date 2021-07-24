da6ef3b3c14d59b3bf2d9e83fab6b5d6894806a3
Omar Hosny <68732608+Omar-Hosni@users.noreply.github.com>
2021-07-24 14:18:51 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+# Abstract Class
+class Animal():
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        raise NotImplementedError("Subclass must implement this abstract method")
+
+
+class Dog(Animal):
+    def speak(self):
+        return self.name + " says woof"
+
+class Cat(Animal)
+
+    def speak(self):
+        return self.name + " says meow"
